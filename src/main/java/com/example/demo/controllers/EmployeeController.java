f9739db99d6dbbf9762fc7130235732158253a8a
yei69 <103204281+yei69@users.noreply.github.com>
2023-05-06 16:42:51 -0500

    controller
    actualizado

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controllers;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,8 +20,9 @@ import com.example.demo.services.EmployeeService;
 
 @RestController
 @RequestMapping("/employee")
-public class EmployeeController {
+public class EmployeeController<fecha, Employee> {
 
+    private static final String STRING = "";
     @Autowired
     EmployeeService employeeService;
 
@@ -25,7 +32,40 @@ public class EmployeeController {
     }
 
     @PostMapping
-    public EmployeeModel safeEmployee(@RequestBody EmployeeModel employee){
+    public EmployeeModel safetEmployee(@RequestBody EmployeeModel employee){
         return this.employeeService.safeEmployee(employee);
     }
-}
+
+    @PostMapping("/employees")
+    public ResponseEntity<Employee> Employeemodel(@Validated @RequestBody Employee employee) {
+        // Código para guardar el empleado en la base de datos
+        // ...
+    }
+    
+    /**
+     *
+     */
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    
+    try {
+        String fechaString;
+        Date fecha = (Date) sdf.parse(fechaString);
+    } catch (ParseException ex) {
+         // Fecha inválida, manejo del error
+    }
+    Calendar fechaNacimiento = Calendar.getInstance();
+    fechaNacimiento.setTime(employee.getFechaNacimiento());
+    Calendar ahora = Calendar.getInstance();
+    
+    int diff_anios = ahora.get(Calendar.YEAR) - fechaNacimiento.get(Calendar.YEAR);
+    int diff_meses = ahora.get(Calendar.MONTH) - fechaNacimiento.get(Calendar.MONTH);
+    int diff_dias = ahora.get(Calendar.DAY_OF_MONTH) - fechaNacimiento.get(Calendar.DAY_OF_MONTH);
+    
+    // Ajustar el cálculo de la edad si el mes y el día actual son menores que el mes y el día de nacimiento
+    if (diff_meses < 0 || (diff_meses == 0 && diff_dias < 0)) {
+        diff_anios--;
+    }
+    
+    if (diff_anios < 18) {
+     // El empleado no es mayor de edad, manejo del error
+    }
